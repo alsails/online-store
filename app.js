bec1ca4593474743f38aa65adfaa8ac49e119733
Your Name <mash.pogonina2014@yandex.ru>
2023-04-26 18:27:45 +0300

    Подключение к бд

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+
+const { PORT = 3000 } = process.env;
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+const app = express();
+
+mongoose
+    .connect('mongodb://127.0.0.1/onlineStoreDB');
+
+app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT);
